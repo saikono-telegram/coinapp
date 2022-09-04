f56a91c10c90acc8570d842a12387c41f317e92d
Saikono <111527317+saikono-telegram@users.noreply.github.com>
2022-09-04 17:52:20 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,16 +5,8 @@ tg.expand();
 let btn = document.getElementById("btn"); 
 
 btn.addEventListener('click', function(){ 
-   if (tg.MainButton.isVisible){ 
-      let coincard = document.getElementById("coincard");
-      let converter = document.createElement('p'); 
-      converter.innerText = `TEST`; 
-      coincard.appendChild(converter);
-   }
-   else{ 
-      let coincard = document.getElementById("coincard");
-      let converter = document.createElement('p'); 
-      converter.innerText = `TEST`; 
-      coincard.appendChild(converter);
-   }
+    let coincard = document.getElementById("coincard");
+     let converter = document.createElement('p'); 
+     converter.innerText = `TEST`; 
+     coincard.appendChild(converter);
 });
