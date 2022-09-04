3ca09bdf73fe82acc988c226e4211236bb8c5a7c
Saikono <111527317+saikono-telegram@users.noreply.github.com>
2022-09-04 18:45:50 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,18 +8,20 @@ btn.addEventListener('click', function(){
 	const getResource = async(url) => {
         const res = await fetch(url);
         const body = await res.json();  
+        converter.innerText = `${body}`
+        coincard.appendChild(converter);    
         return body;
     };
 
     getResource('https://www.cbr-xml-daily.ru/daily_jsonp.js')
-        .then((body)=>{
-            const usdCur = body[9].Value;
-            const eurCur = body[10].Value;
+        //.then((body)=>{
+            //const usdCur = body[9].Previous;
+            //const eurCur = body[10].Previous;
 
-            let coincard = document.getElementById("coincard");
-            let converter = document.createElement('p');
-            converter.innerText = `${usdCur}\n${eurCur}`
-            coincard.appendChild(converter);            
+            //let coincard = document.getElementById("coincard");
+            //let converter = document.createElement('p');
+            //converter.innerText = `${usdCur}\n${eurCur}`
+            //coincard.appendChild(converter);            
     
-        });
+       //});
 });
