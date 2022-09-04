6087f50b1c62e37b1dac0bb022e78a54f23493df
Saikono <111527317+saikono-telegram@users.noreply.github.com>
2022-09-04 21:10:04 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,17 +2,35 @@ let tg = window.Telegram.WebApp;
 
 tg.expand(); 
 
-tg.MainButton.setText(`TEST`); 
-tg.MainButton.textColor = "#FFF"; 
-tg.MainButton.color = "#ffd800"; 
+const getResource = async(url) => {
+    const res = await fetch(url);
+    const body = await res.json();  
+    return body;
+};
 
-let btn = document.getElementById("convert"); 
+getResource('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then((body)=>{
+        const summ = document.getElementById('summ').value;
 
-btn.addEventListener('click', function(){ 
-   if (tg.MainButton.isVisible){ 
-      tg.MainButton.hide() 
-   }
-   else{ 
-      tg.MainButton.show() 
-   }
-});
+        const usdCur = Math.floor(summ / body.Valute.USD.Value);
+        const eurCur = Math.floor(summ / body.Valute.EUR.Value);
+
+        tg.MainButton.setText(`${summ}₽ = ${usdCur}$ | ${eurCur}€`); 
+        tg.MainButton.textColor = "#0a0a0a"; 
+        tg.MainButton.color = "#ffd800"; 
+
+        let btn = document.getElementById("convert"); 
+
+        btn.addEventListener('click', function(){ 
+           if (tg.MainButton.isVisible)
+           { 
+               tg.MainButton.hide() 
+           }
+
+           else
+           { 
+              tg.MainButton.show() 
+           }
+
+        });
+    });
