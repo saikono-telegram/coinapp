2d637b648115f2d750efc5a39d6bfde7bcb3fc40
Saikono <111527317+saikono-telegram@users.noreply.github.com>
2022-09-04 18:13:51 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,8 +5,13 @@ tg.expand();
 let btn = document.getElementById("btn"); 
 
 btn.addEventListener('click', function(){ 
-    let coincard = document.getElementById("coincard");
-     let converter = document.createElement('p'); 
-     converter.innerText = `TEST`; 
-     coincard.appendChild(converter);
+	axios.get("https://www.cbr-xml-daily.ru/daily_json.js")
+    .then((res) => { 
+    	let coincard = document.getElementById("coincard");
+        let converter = document.createElement('p'); 
+        valute.EUR = res.data.Valute.EUR.Value
+        valute.USD = res.data.Valute.USD.Value
+        converter.innerText = `${valute.EUR}\n${valute.USD}`
+        coincard.appendChild(converter);   
+    })
 });
