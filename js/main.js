8ea2ef1d6fd01570d3bf7a57b328b377ff18b937
Saikono <111527317+saikono-telegram@users.noreply.github.com>
2022-09-04 20:16:50 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,29 +4,31 @@ tg.expand();
 
 let btn = document.getElementById("btn"); 
 
-const getResource = async(url) => {
-    const res = await fetch(url);
-    const body = await res.json();  
-    return body;
-};
-
-getResource('https://www.cbr-xml-daily.ru/daily_json.js')
-    .then((body)=>{
-        const summ = document.getElementById('summ').value;
-
-        const usdCur = Math.floor(summ / body.Valute.USD.Value);
-        const eurCur = Math.floor(summ / body.Valute.EUR.Value);
-
-        tg.MainButton.setText(`${summ}₽ = ${usdCur}$ | ${eurCur}€!`); 
-        tg.MainButton.textColor = "#FFF"; 
-        tg.MainButton.color = "#ffd800"; 
-   });
-
 btn.addEventListener('click', function(){ 
-   if (tg.MainButton.isVisible){ 
-      tg.MainButton.hide() 
-   }
-   else{ 
-      tg.MainButton.show()       
-   }
+   const getResource = async(url) => {
+       const res = await fetch(url);
+       const body = await res.json();  
+       return body;
+   };
+
+   getResource('https://www.cbr-xml-daily.ru/daily_json.js')
+      .then((body)=>{
+
+          const summ = document.getElementById('summ').value;
+
+          const usdCur = Math.floor(summ / body.Valute.USD.Value);
+          const eurCur = Math.floor(summ / body.Valute.EUR.Value);
+
+          tg.MainButton.setText(`${summ}₽ = ${usdCur}$ | ${eurCur}€!`); 
+          tg.MainButton.textColor = "#FFF"; 
+          tg.MainButton.color = "#ffd800";
+
+          if (tg.MainButton.isVisible) { 
+             tg.MainButton.hide() 
+          }
+
+          else { 
+             tg.MainButton.show() 
+          }
+     });      
 });
